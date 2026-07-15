1c66157def6b58c7b0b4297cf88f6b596f07f16f
ychda <ychda@qq.com>
2026-07-15 22:15:59 +0800

    footer 保持在底部

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js,ts,tsx}"]
+}
+
+/**
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+*/
